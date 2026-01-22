f5fe1d6de6507322dd1b4301bbd2bd6464fbaf15
Rayyan <Rayyanshafqat@gmail.com>
2026-01-22 21:52:42 +0500

    initial commit

---
+++ Python first Project.py
@@ -0,0 +1,12 @@
+# printing a greeting message
+print ("Hello everyone!")
+ 
+# printing my name
+print ("My name is Rayyan.")
+
+# printing a fun fact about myself
+# print("I love playing Snooker.")
+print ("I love playing Snooker.")
+
+# printing performing a simple calculation
+print ("The result of 25 + 20 is:",25 + 20)
