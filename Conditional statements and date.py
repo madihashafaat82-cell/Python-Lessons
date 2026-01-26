29b7bacc0f579e6cdadc27797bc80948e9bb5770
Rayyan <Rayyanshafqat@gmail.com>
2026-01-26 22:43:17 +0500

    initial commit

---
+++ Conditional statements and date.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number to check the following"))
+print ("Number to be checked",number)
+if number %2==0:
+    print("This is an even number")
+else :
+    print("this is an odd number")
