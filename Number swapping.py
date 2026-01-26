002a9ca755191af92e314306a27773f18c22b4b2
Rayyan <Rayyanshafqat@gmail.com>
2026-01-26 23:33:37 +0500

    initial

---
+++ Number swapping.py
@@ -0,0 +1,8 @@
+x = input ("The value of x:")
+y = input ("The value of y:")
+z = input ("The value of z:")
+# swapping
+x > y
+y > z
+# Displaying results after swapping
+print ("Value in Ascending order:",z,y,x)
