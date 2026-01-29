7e633c1bc78d2ad7dadda231cacaebde125d0865
Rayyan <Rayyanshafqat@gmail.com>
2026-01-29 22:54:52 +0500

    initial

---
+++ Conditional statement and data 3.py
@@ -0,0 +1,10 @@
+x = int(input('Enter a number to be check:'))
+if x > 50:
+    print('It is greater than 50')
+    if x/50:
+        print('It is even.')
+    else:
+        print('It is odd.')
+else:
+    print('It is less than 50.')
+
