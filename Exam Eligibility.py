f528c5aedaa27ff8244fe47a892b3bd9d396bbfa
Rayyan <Rayyanshafqat@gmail.com>
2026-01-29 22:09:43 +0500

    initial

---
+++ Exam Eligibility.py
@@ -0,0 +1,6 @@
+age = int(input('Enter an age to be check'))
+print('Is eligible for exam of 9 class')
+if age == 15:
+ print ('Yes, it is Eligible for exam.')
+else:
+ print('No, it is Eligible for exam.')
