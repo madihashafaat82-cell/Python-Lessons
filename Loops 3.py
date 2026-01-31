d4b85b8606cc3f47b2b2918eedb3e43fcb840f6a
Rayyan <Rayyanshafqat@gmail.com>
2026-01-31 22:13:49 +0500

    initial

---
+++ Loops 3.py
@@ -0,0 +1,4 @@
+# Python program to calculate the sum of the first Twenty odd numbers
+# Use built-in sum and range functions to calculate the sum
+total_sum = sum(range(1, 21))
+print(f"The sum of the first twenty odd numbers is {total_sum}")
