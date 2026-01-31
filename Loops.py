e772d335a9466c8cf5df92432f6716fb88e5f13d
Rayyan <Rayyanshafqat@gmail.com>
2026-01-31 21:57:50 +0500

    initial

---
+++ Loops.py
@@ -0,0 +1,5 @@
+# Python program to print the multiplication table of 45 from 1 to 10
+# Loop from 1 to 10
+for i in range(1,11):
+    # Print the multiplication of 45 by i
+    print(f"45 x {i} = {45 * i}")
