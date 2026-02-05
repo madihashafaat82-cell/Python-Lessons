d8890af5bcca3d70c27c62d3d2be208be232a74f
Rayyan <Rayyanshafqat@gmail.com>
2026-02-05 17:11:21 +0500

    initial

---
+++ Intro to Python 1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello!")
+
+# Printing my name
+print("My name is Rayyan.")
+
+# Printing a fun fact about myself
+# print("I love playing soccer.")
+print("I love playing Football.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
