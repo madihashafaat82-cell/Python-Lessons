6fe7eab2248b3b6994e6de5f370ae3f5da40c40e
Rayyan <Rayyanshafqat@gmail.com>
2026-02-12 22:40:09 +0500

    initial

---
+++ Practice of Turtle.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("Dark green")
+sc = turtle.Screen()
+sc.setup(500,500)
+turtle.title("Welcome to the PC")
+board = turtle.Turtle()
+for i in range(3):
+ board.forward(90)
+ board.left(120)
+for i in range(4):
+ board.penup
+ board.forward(90)
+ board.left(120)
+ i =i+1
+ 
+turtle.done()
+
