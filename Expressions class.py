a30bba72b29e9cf349cd777e77a5c528f58ecbe2
Rayyan <Rayyanshafqat@gmail.com>
2026-02-24 00:06:09 +0500

    initial

---
+++ Expressions class.py
@@ -0,0 +1,34 @@
+print('Hello,my name is Rayyan.I am the student of the codingal.')
+print('Today,We make a project in which we make constructer and object and then calling the possible functions.')
+print('Now,I write the code for this project.')
+print('The code are as follows:')
+class expression:
+
+    #constructer
+    def __init__(self,num1,num2,num3,num4,num5,num6):
+        self.num1 = num1
+        self.num2 = num2
+        self.num1 = num3
+        self.num2 = num4
+        self.num1 = num5
+        self.num2 = num6
+
+#Object creation
+num1 = 2
+num2 = 6
+num3 = 3
+num4 = 7
+num5 = 8
+num6 = 10
+
+#Calling function
+print(num1+num2)
+print(num6*num5)
+print(num2/num1)
+print(num1^num3)
+print(num4*num5)
+print(num3+num4)
+
+#Ending
+print('I hope that you can understand this code.We met in the next project.')
+print('Allah Hafiz')
