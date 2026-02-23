e6cff2056629ce16fb1cfff0186011f43758cd2d
Rayyan <Rayyanshafqat@gmail.com>
2026-02-23 22:56:53 +0500

    initial

---
+++ Object oriented programming -2 1.py
@@ -0,0 +1,19 @@
+class fruit:
+
+    #constructer
+    def __init__(self , name , color):
+        self.name = name
+        self.color = color
+#object creation
+apple = fruit('Watermelon','green')
+
+#Calling funtion
+print(apple.name)
+print(apple.color)
+print(apple.name,apple.color)
+    
+
+
+
+
+
---
+++ object oriented programming-2 3.py
@@ -0,0 +1,11 @@
+class employee:
+
+    #constructer
+    def __init__(self):
+        print('Constructer are created.' )
+    #destructer
+    def __del__(self):
+        print('How to make a destructer.')
+
+obj =employee()
+del obj
