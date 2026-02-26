8949f7fdcdd5ba3ff4db95aad62660c83d34d478
Rayyan <Rayyanshafqat@gmail.com>
2026-02-26 22:35:43 +0500

    initial

--- Number swapping.py
+++
@@ -1,8 +0,0 @@
-x = input ("The value of x:")
-y = input ("The value of y:")
-z = input ("The value of z:")
-# swapping
-x > y
-y > z
-# Displaying results after swapping
-print ("Value in Ascending order:",z,y,x)
--- Tuple to list.py
+++
@@ -1,8 +0,0 @@
-# Tuple
-Tuple = (1,3,4,5,6,7,2,3,10)
-
-
-c_list = list(Tuple)
-
-print("Original Tuple:",Tuple)
-print("Converted list" , c_list)
